c11b8bf42d0c0d2687dbc8a1302b852623c0f4e9
VirtualGinger <redheadduggan@gmail.com>
2025-09-21 09:46:59 -0700

    Moving w/o animation

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -2,8 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player_Movement
+public class Player_Movement : MonoBehaviour
 {
     public float speed = 5;
     public Rigidbody2D rb;
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        rb.linearVelocity = new Vector2(horizontal, vertical) * speed;
+    }
 }
