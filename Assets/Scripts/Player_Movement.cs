c96d94ecf0a12119cfc4007bdde4d47b67f6f10c
VirtualGinger <redheadduggan@gmail.com>
2025-09-21 22:31:36 -0700

    Refined Move w/o anim

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player_Movement : MonoBehaviour
 {
-    public float speed = 5;
+    public float moveSpeed = 5f;
     public Rigidbody2D rb;
+    Vector2 movementt;
 
-
+    void Update()
+    {
+        movementt.x = Input.GetAxisRaw("Horizontal");
+        movementt.y = Input.GetAxisRaw("Vertical");
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
-        rb.linearVelocity = new Vector2(horizontal, vertical) * speed;
+        rb.MovePosition(rb.position + movementt * moveSpeed * Time.fixedDeltaTime);
     }
 }
