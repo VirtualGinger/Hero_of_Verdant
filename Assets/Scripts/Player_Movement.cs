48c27db60bc65c3e6ec90ce376a2815e1d567931
VirtualGinger <redheadduggan@gmail.com>
2025-09-23 22:14:58 -0700

    Added animations to movement

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -7,16 +7,26 @@ public class Player_Movement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
-    Vector2 movementt;
+    public Animator animator;
+    Vector2 movement;
+    private Vector2 LastDirection = Vector2.down;
 
     void Update()
     {
-        movementt.x = Input.GetAxisRaw("Horizontal");
-        movementt.y = Input.GetAxisRaw("Vertical");
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        if (movement != Vector2.zero){
+            LastDirection = movement;
+        }
+
+        animator.SetFloat("Horizontal", LastDirection.x);
+        animator.SetFloat("Vertical", LastDirection.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movementt * moveSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
