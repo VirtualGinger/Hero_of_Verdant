40ba4e9be30b5fb0e239275614274982e85ea958
antjromero <18187862+antjromero@users.noreply.github.com>
2025-10-12 22:46:16 -0700

    added footsteps and first dungeon warp point

---
+++ Assets/Scripts/Sounds/Footsteps.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Footsteps : MonoBehaviour
+{
+    public AudioSource footstepsSound;
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            footstepsSound.enabled = true;
+        }
+        else
+        {
+            footstepsSound.enabled = false;
+        }
+
+    }
+}
