274709bc40886141e0a827cc63f4cfb6a2da5c9d
antjromero <18187862+antjromero@users.noreply.github.com>
2025-11-01 19:06:38 -0700

    added player weapon hit detection

--- Assets/Scripts/Player/Player_Combat.cs
+++ Assets/Scripts/Player/Player_Combat.cs
@@ -6,6 +6,11 @@ public class Player_Combat : MonoBehaviour
     public float cooldown = 1;
     private float timer;
 
+
+    public GameObject attackPoint;
+    public float radius;
+    public LayerMask enemies;
+
     private void Update()
     {
         if (timer > 0)
@@ -24,10 +29,29 @@ public class Player_Combat : MonoBehaviour
             anim.SetBool("IsAttacking", true);
             timer = cooldown;
         }
+
+        Collider2D[] enemy = Physics2D.OverlapCircleAll(attackPoint.transform.position, radius, enemies);
+
+        foreach (Collider2D enemyGameobject in enemy)
+        {
+            Debug.Log("Ememy Hit");
+        }
     }
+
+//public void attack()
+  //  {
+
+//    }
+
+
 
     public void FinishAttack()
     {
         anim.SetBool("IsAttacking", false);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(attackPoint.transform.position, radius);
     }
 }
