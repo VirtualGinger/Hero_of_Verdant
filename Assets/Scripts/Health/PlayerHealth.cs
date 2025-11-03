e282e4a06531bb714992cd0a4e35dab49c13db9b
antjromero <18187862+antjromero@users.noreply.github.com>
2025-11-03 13:12:14 -0800

    updated health bar to work with new persistent character, updated skeleton physics

--- Assets/Scripts/Health/PlayerHealth.cs
+++ Assets/Scripts/Health/PlayerHealth.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class PlayerHealth : MonoBehaviour
 {
     public float health;
     public float maxHealth;
     public Image HealthBar;
+
+    private bool isDead = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,10 +20,17 @@ public class PlayerHealth : MonoBehaviour
     {
         HealthBar.fillAmount = Mathf.Clamp(health / maxHealth, 0, 1);
 
-        if(health <= 0)
-        {
-            Destroy(gameObject);
-            Debug.Log("Player has died.");
+        if(health <= 0 && !isDead)
+        { 
+            isDead = true;
+            StartCoroutine(DieSequence(0.3f));
         }
+    }
+
+    IEnumerator DieSequence(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Debug.Log("Player has died.");
+        Destroy(gameObject);
     }
 }
