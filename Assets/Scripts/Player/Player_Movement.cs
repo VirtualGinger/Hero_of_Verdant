49a843953bd55a477b892d68cbb283f1ad3ba89e
VirtualGinger <redheadduggan@gmail.com>
2025-11-13 22:40:12 -0800

    Added dash to movement script and animations, not bound to player yet

--- Assets/Scripts/Player/Player_Movement.cs
+++ Assets/Scripts/Player/Player_Movement.cs
@@ -6,6 +6,10 @@ using UnityEngine.InputSystem;
 public class Player_Movement : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float dashSpeed = 10f;
+    bool isDashing = false;
+    float dashDuration = 1f;
+    float dashCooldown = 1f;
     public Rigidbody2D rb;
     public Animator animator;
     Vector2 movement;
@@ -15,15 +19,26 @@ public class Player_Movement : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetButtonDown("Slash"))
-        {
-            player_Combat.Attack(LastDirection);
-        }
+    if (Input.GetButtonDown("Slash"))
+    {
+        player_Combat.Attack(LastDirection);
+    }
+
+    if (Input.GetButtonDown("Dash") && dashCooldown <= 0f && !isDashing)
+    {
+        StartCoroutine(Dash());
+    }
+
+    if (dashCooldown > 0f)
+    {
+        dashCooldown -= Time.deltaTime;
+    }
     }
 
 
     void FixedUpdate()
     {
+        if (isDashing) return;
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
@@ -38,5 +53,23 @@ public class Player_Movement : MonoBehaviour
         animator.SetFloat("Vertical", LastDirection.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    IEnumerator Dash()
+{
+    isDashing = true;
+
+    Vector2 dashDirection = LastDirection.normalized;
+
+    while (dashDuration > 0f)
+    {
+        rb.MovePosition(rb.position + dashDirection * dashSpeed * Time.fixedDeltaTime);
+        dashDuration -= Time.fixedDeltaTime;
+        yield return new WaitForFixedUpdate();
+    }
+
+    isDashing = false;
 }
+
+
 }
