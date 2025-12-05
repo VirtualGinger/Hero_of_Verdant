952d9b25e7f0f21b3b4cc51af9031259550c2bb2
antjromero <18187862+antjromero@users.noreply.github.com>
2025-12-05 02:31:14 -0800

    powerup message, boss dungeon music, dungeon two balance

--- Assets/Scripts/Player/DashBoostPickup.cs
+++ Assets/Scripts/Player/DashBoostPickup.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using TMPro;
+using System.Collections;
 
 public class DashBoostPickup : MonoBehaviour
 {
@@ -15,9 +17,28 @@ public class DashBoostPickup : MonoBehaviour
 
             // 2. Attach it to the player (make it a child)
             newLogicObject.transform.SetParent(collision.transform);
-            
-            // 3. Destroy this pickup from the world
+
+            // 3. Update the TextMeshPro UI named "Warning"
+            GameObject warningObj = GameObject.Find("Warning");
+            if (warningObj != null)
+            {
+                TextMeshProUGUI warningText = warningObj.GetComponent<TextMeshProUGUI>();
+                if (warningText != null)
+                {
+                    warningText.text = "Dash speed increased.";
+                    // Run coroutine on the Warning object instead of this pickup
+                    warningObj.GetComponent<MonoBehaviour>().StartCoroutine(ClearMessageAfterDelay(warningText, 3f));
+                }
+            }
+
+            // 4. Destroy this pickup from the world
             Destroy(gameObject);
         }
+    }
+
+    private IEnumerator ClearMessageAfterDelay(TextMeshProUGUI textObj, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        textObj.text = string.Empty;
     }
 }
